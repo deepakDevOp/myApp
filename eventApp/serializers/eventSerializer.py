ce119a409e631793afaf4cf001d3eb6f1550eae2
deepakDevOp <deepak.513770@gmail.com>
2024-05-27 18:48:24 +0530

    modified event images apis

--- eventApp/serializers/eventSerializer.py
+++ eventApp/serializers/eventSerializer.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from eventApp.models import Event
 from .validators import EventValidatorMixin
-from eventApp.utils import upload_image_to_s3
+from eventApp.utils import upload_image_to_s3,delete_image_s3
 
 
 class EventSerializer(serializers.ModelSerializer):
@@ -38,19 +38,28 @@ class UpdateEventSerializer(EventValidatorMixin, serializers.ModelSerializer):
     def update(self, instance, validated_data):
         request = self.context.get('request')
         pictures = request.FILES.getlist("pic", None)
-        uploaded_images = []
+        updated_image_urls = instance.image_urls
         for image in pictures:
             eventid = validated_data.get("eventid")
             image_url, file_name = upload_image_to_s3(image_data=image, event_id=eventid)
             image_data = {"image_id": file_name,
                           "image_url": image_url}
-            uploaded_images.append(image_data)
-        validated_data["image_urls"] = uploaded_images
+            updated_image_urls.append(image_data)
+        remove_ids = request.GET.get("remove_ids", None)
+        if remove_ids:
+            remove_ids = remove_ids.split(",")
+            delete_image_s3(remove_ids=remove_ids, eventid=request.data.get("eventid"))
+            for image_id in remove_ids:
+                for img_obj_index in range(len(updated_image_urls)):
+                    if image_id in updated_image_urls[img_obj_index].values():
+                        del updated_image_urls[img_obj_index]
+                        break
         # Update the remaining fields
         for key, value in validated_data.items():
             setattr(instance, key, value)
 
         # Save the instance
+        instance.image_urls = updated_image_urls
         instance.save()
         return instance
 
