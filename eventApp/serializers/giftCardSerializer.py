ca6c3f4f75ce8f67d0a21a333977abcf8c17d3f4
deepakDevOp <deepak.513770@gmail.com>
2024-07-28 01:37:29 +0530

    added images for giftcards

--- eventApp/serializers/giftCardSerializer.py
+++ eventApp/serializers/giftCardSerializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from eventApp.models import GiftCardsList
+from userPolls.models import MediaFile
 
 
 class GiftCardsListSerializer(serializers.ModelSerializer):
     class Meta:
         model = GiftCardsList
         fields = "__all__"
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        image_id = ret.get("image")
+        media_file = MediaFile.objects.get(file_id=image_id)
+        ret["image"] = media_file.file_url
+        return ret
+
